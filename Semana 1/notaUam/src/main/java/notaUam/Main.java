688190b7ec587cd6ce63588a5eec009570cb3906
KendrickBarrios <kkbarrios@uamv.edu.ni>
2024-08-25 12:14:14 -0600

    Se agrega la carpeta Semana 1, con el ejercicio notaUAM desarrollado durante la sesion 2.

---
+++ Semana 1/notaUam/src/main/java/notaUam/Main.java
@@ -0,0 +1,44 @@
+package notaUam;
+
+import modelos.Estudiante;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\u001B[32m");
+        System.out.println("UAM");
+        // crear objeto estudiante
+        Estudiante est = new Estudiante();
+        String nombres, apellidos, cif, asignatura;
+        int nota1, nota2, nota3, nf;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa los siguientes datos: ");
+        System.out.print("CIF: ");
+        cif = sc.nextLine();
+        System.out.print("Nombres: ");
+        nombres = sc.nextLine();
+        System.out.print("Apellidos: ");
+        apellidos = sc.nextLine();
+        System.out.print("Asignatura: ");
+        asignatura = sc.nextLine();
+        System.out.print("Corte 1: ");
+        nota1 = Integer.parseInt(sc.nextLine());
+        System.out.print("Corte 2: ");
+        nota2 = Integer.parseInt(sc.nextLine());
+        System.out.print("Corte 3: ");
+        nota3 = Integer.parseInt(sc.nextLine());
+
+        est.setNombres(nombres);
+        est.setApellidos(apellidos);
+        est.setAsignatura(asignatura);
+        est.setCorte1(nota1);
+        est.setCorte2(nota2);
+        est.setCorte3(nota3);
+
+        nf = est.obtNotaFinal();
+        System.out.println("Nota final: " + nf);
+        System.out.println(est.evalNotaFinal());
+        System.out.println(est.evalConvocatoria());
+    }
+}
