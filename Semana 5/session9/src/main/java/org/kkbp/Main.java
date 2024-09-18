682c31f05dc88b7f2725ef2ecc5c634cbfd56e95
KendrickBarrios <kkbarrios@uamv.edu.ni>
2024-09-17 18:26:58 -0600

    Se crea el menu en la clase Main

--- Semana 5/session9/src/main/java/org/kkbp/Main.java
+++ Semana 5/session9/src/main/java/org/kkbp/Main.java
@@ -3,13 +3,16 @@ package org.kkbp;
 import org.kkbp.dao.StudentDao;
 import org.kkbp.models.Student;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Student student1 = new Student("Kendrick", 23, "kendrick@gmail.com");
+        StudentDao students = new StudentDao();
+        /* Student student1 = new Student("Kendrick", 23, "kendrick@gmail.com");
         Student student2 = new Student("Luis", 25, "luis@gmail.com");
         Student student3 = new Student("Maria", 22, "maria@gmail.com");
 
-        StudentDao students = new StudentDao();
+
         if(students.store(student1)) {
             System.out.println("Student stored correctly.");
         } else {
@@ -17,6 +20,45 @@ public class Main {
         }
         students.store(student2);
         students.store(student3);
-        students.showStudents();
+        students.showStudents(); */
+
+        Scanner sc = new Scanner(System.in);
+        int option = 0;
+        String menu = "1. Add student\n" +
+                "2. Find student\n" +
+                "3. Show students\n" +
+                "4. Exit program\n" +
+                "Enter you choice: ";
+        while (option != 4) {
+            System.out.println(menu);
+            option = sc.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.println("Enter the name of the student: ");
+                    String name = sc.next();
+                    System.out.println("Enter the age of the student: ");
+                    int age = sc.nextInt();
+                    System.out.println("Enter the email address of the student: ");
+                    String email = sc.next();
+                    Student student = new Student(name, age, email);
+                    students.store(student);
+                    break;
+                case 2:
+                    System.out.println("Enter the name of the student: ");
+                    String nameToFind = sc.next();
+                    students.findByName(nameToFind);
+                    break;
+                case 3:
+                    students.showStudents();
+                    break;
+                case 4:
+                    System.out.println("Thank you for using the program!");
+                    break;
+                default:
+                    System.out.println("Please enter a valid option.");
+                    break;
+            }
+        }
     }
 }
